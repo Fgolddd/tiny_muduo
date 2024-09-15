a6ac9d6bedf38273a19686981f0267c22576abf8
lyx <isyanxin.lin@gmail.com>
2024-09-15 16:00:52 +0800

    class EventLoopThreadPool done

--- EventLoopThread.cpp
+++ EventLoopThread.cpp
@@ -1,11 +1,8 @@
 #include "EventLoopThread.h"
 #include "EventLoop.h"
 
-#include <mutex>
-
-EventLoopThread::EventLoopThread(
-    const ThreadInitCallback &cb = ThreadInitCallback(),
-    const std::string &name)
+EventLoopThread::EventLoopThread(const ThreadInitCallback &cb,
+                                 const std::string &name)
     : loop_(nullptr), exiting_(false),
       thread_(std::bind(&EventLoopThread::threadFunc, this), name), mutex_(),
       cond_(), callback_(cb) {}
---
+++ EventLoopThreadPoll.cpp
@@ -0,0 +1,43 @@
+#include "EventLoopThreadPoll.h"
+#include "EventLoopThread.h"
+#include <memory>
+EventLoopThreadPoll::EventLoopThreadPoll(EventLoop *baseLoop,
+                                         const std::string &nameArg)
+    : baseLoop_(baseLoop), name_(nameArg), started_(false), numThreads_(0),
+      next_(0) {}
+EventLoopThreadPoll::~EventLoopThreadPoll() {}
+
+void EventLoopThreadPoll::start(
+    const ThreadInitCallback &cb = ThreadInitCallback()) {
+    started_ = true;
+    for (int i = 0; i < numThreads_; i++) {
+        char buf[name_.size() + 32];
+        snprintf(buf, sizeof buf, "%s%d", name_.c_str(), i);
+        EventLoopThread *t = new EventLoopThread(cb, buf);
+        threads_.push_back(std::unique_ptr<EventLoopThread>(t));
+        // 创建线程，绑定一个新的EventLoop
+        loops_.push_back(t->startLoop());
+    }
+    // 只有baseloop1个线程
+    if (numThreads_ == 0 && cb) {
+        cb(baseLoop_);
+    }
+}
+
+// 轮询方式分配channel
+EventLoop *EventLoopThreadPoll::getNextLoop() {
+    EventLoop *loop = baseLoop_;
+
+    if (loops_.size()) {
+        loop = loops_[next_++];
+        if (next_ >= loops_.size()) next_ = 0;
+    }
+    return loop;
+}
+std::vector<EventLoop *> EventLoopThreadPoll::getAllLoops() {
+    if (loops_.empty()) {
+        return std::vector<EventLoop *>(1, baseLoop_);
+    } else {
+        return loops_;
+    }
+}
